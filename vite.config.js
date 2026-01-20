c4991d5f374a5714b8daea47b745b14eca367556
Filip <filip.bergander1996@gmail.com>
2026-01-20 13:27:32 +0100

    Navigeringsmeny responsiv på mindre enheter.

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
     build: {
         rollupOoptions: {
             input: {
-                main: resolve(__dirnamn, "index.html"),
-                pictures: resolve(__dirname, "pictures.html")
+                main: resolve(__dirname, "index.html"),
+                pictures: resolve(__dirname, "pictures.html"),
                 publish: resolve(__dirname, "publish.html")
             }
         }
     }
-})
+});
