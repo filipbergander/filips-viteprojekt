6a5fab2040e58f0278666e30d0dea94ac2a7162f
Filip <filip.bergander1996@gmail.com>
2026-01-20 20:50:06 +0100

    Fem bilder upplagd. Automatiserat processen med att förbättra bildernas filstorlek genom vite-plugin-image-optimizer och sharp.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
     build: {
-        rollupOoptions: {
+        rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
                 pictures: resolve(__dirname, "pictures.html"),
                 publish: resolve(__dirname, "publish.html")
             }
         }
-    }
+    },
+    plugins: [
+        ViteImageOptimizer({
+            png: {
+                quality: 65
+            },
+            jpeg: {
+                quality: 65
+            },
+            jpg: {
+                quality: 65
+            },
+            avif: {
+                quality: 65
+            }
+        })
+    ]
 });
